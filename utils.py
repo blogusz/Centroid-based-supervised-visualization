f440105a61f4e5d339783df24d4dbdcf8858aaee
Bogusz Laszczyk <laszczykb@student.agh.edu.pl>
2023-06-18 18:07:39 +0200

    File saving for everything + cosmetic changes

--- utils.py
+++ utils.py
@@ -1,13 +1,24 @@
 import os
 
 
-def create_results_folders(algorithm: str, method: str) -> tuple:
-    results_folder = f"stored_results/{algorithm}/{method}"
-    centroids_folder = os.path.join(results_folder, "centroids")
-    labels_folder = os.path.join(results_folder, "labels")
+def create_algorithm_directory(algorithm: str, method: str) -> tuple:
+    centroids_folder = os.path.join(f"stored_results/{algorithm}/{method}", "centroids")
+    labels_folder = os.path.join(f"stored_results/{algorithm}/{method}", "labels")
 
-    os.makedirs(results_folder, exist_ok=True)
     os.makedirs(centroids_folder, exist_ok=True)
     os.makedirs(labels_folder, exist_ok=True)
 
     return centroids_folder, labels_folder
+
+
+def create_directory(
+    algorithm: str, method: str, type: str, umap_tsne: str = ""
+) -> str:
+    folder = f"stored_results/{algorithm}/{method}"
+    if umap_tsne:
+        folder = os.path.join(folder, umap_tsne)
+    folder = os.path.join(folder, type)
+
+    os.makedirs(folder, exist_ok=True)
+
+    return folder
